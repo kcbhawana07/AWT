ce1eb60550f198182ee4a8ea99cd79273ca95044
Bhawana KC <bhawanakc@Bhawana-KC.local>
2025-05-14 07:50:05 +0545

    using simple js function

---
+++ Backend/Backend/calculator.js
@@ -0,0 +1,45 @@
+
+const abc="software";
+
+let name="s";
+const roll=2;
+
+const names=["Sony","Diya","Priya","Dipa"]
+for (let i=0;i<names.length;i++){
+    console.log(names[i]);
+}
+
+var a=10;
+var b=5;
+function add(){
+    console.log(a+b);
+}
+add()
+
+function sub(){
+    console.log(a-b);
+}
+sub()
+
+function mul(){
+    console.log(a*b);
+}
+mul()
+
+function div(){
+    console.log(a/b);
+}
+div()
+
+
+function addition(c,d){
+    console.log(c+d);
+}
+addition(7,9);
+
+
+
+
+
+
+
